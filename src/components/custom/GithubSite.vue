22f581e93a976c08f1e770298b57fdf4a9121334
YuQisam <100015070+YuQisam@users.noreply.github.com>
2023-03-19 02:44:23 +0800

    feat: 添加新功能
    feat: 添加新功能

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -2,7 +2,7 @@
   <div class="text-neutral-400">
     <span>普通用户 剩余5次</span>
     <a href="@/assets/qrcode.jpg" target="_blank" rel="noopener noreferrer">
-      <img src="@/assets/qrcode.jpg" alt="二维码" onclick="window.open('assets/qrcode.jpg', '_blank', 'width=500,height=500')">
+      <img src="assets/qrcode.jpg" alt="二维码" onclick="window.open('assets/qrcode.jpg', '_blank', 'width=500,height=500')">
     </a>
   </div>
 </template>
