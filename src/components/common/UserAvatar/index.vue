e4422bd83afc74ca71966652dea4424d1eb02de6
YuQisam <100015070+YuQisam@users.noreply.github.com>
2023-03-19 02:38:07 +0800

    feat: 添加新功能
    feat: 添加新功能

--- src/components/common/UserAvatar/index.vue
+++ src/components/common/UserAvatar/index.vue
@@ -8,9 +8,9 @@ import { GithubSite } from '@/components/custom'
       <img class="object-cover" src="@/assets/avatar.jpg" alt="avatar">
     </div>
     <div class="ml-2">
-      <h2 class="font-bold text-md">
+      <!-- <h2 class="font-bold text-md">
         YuQi
-      </h2>
+      </h2> -->
       <p class="text-xs text-gray-500">
         <GithubSite />
       </p>
